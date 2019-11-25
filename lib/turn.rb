8c78752e68669b6aacdfb4ece136e6af4bbf19ba
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2019-11-25 12:15:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -29,11 +29,19 @@ def position_taken?(board, index)
 end
 
 def turn(board)
+<<<<<<< HEAD
   puts "Please enter 1-9:"
 input = gets.strip
   index = input_to_index(input)
   if (valid_move?(board, index))
     board[index] = "X"
+=======
+  puts "Please enter 1-9"
+input = gets.strip
+  index = input_to_index(input)
+  if (valid_move?(board, index))
+    board[index] = "x"
+>>>>>>> 8f56cb061e6e0edf18eff02176b281dd7f994534
     puts "I entered #{input} in response to the gets prompt"
     display_board(board)
   else turn(board)
@@ -41,8 +49,14 @@ input = gets.strip
 end
 
 def move(board, input, type)
+<<<<<<< HEAD
   if (valid_move?(board, input))
     board[input] = type
+=======
+index = input_to_index(input)
+  if (valid_move?(board, index))
+    board[index] = type
+>>>>>>> 8f56cb061e6e0edf18eff02176b281dd7f994534
 
   else turn(board)
   end
